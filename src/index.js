262655bcba0314efa2befb5aedea80646a5fce04
nzf210 <syahril.symd@gmail.com>
2022-04-14 03:01:46 +0800

    update spd 11

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import { store } from './app/store';
 import axios from 'axios';
 axios.defaults.withCredentials = true;
 
-axios.defaults.baseURL = 'http://localhost:3002';
+//axios.defaults.baseURL = 'http://localhost:3002';
 // axios.defaults.baseURL = 'http://202.157.176.42:3002';
-//axios.defaults.baseURL = 'https://appin.id';
+axios.defaults.baseURL = 'https://appin.id';
 // axios.defaults.baseURL = window.Location; yahiko2547
 
 ReactDOM.render(

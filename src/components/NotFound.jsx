31b891728bbe53f72e5c35f69a369abfda0c021c
nzf210 <syahril.symd@gmail.com>
2022-05-16 19:29:57 +0800

    chk2

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -7,7 +7,7 @@ const NotFound = () => {
             <Link to="/home" className="inline-block p-12 rounded-lg text-yellow-50 bg-blue-600 hover:bg-blue-700 text-center justify-center mx-auto pr-8 mt-8 ml-3 items-center my-auto">Back To Home</Link>
 
             <div className="mx-auto items-center w-full min-h-full justify-center text-center my-auto text-red-500 font-bold pt-40">
-                <h1 className="">Halaman Tidak Di Temukan</h1>
+                <h1 className="">Halaman Tidak Ditemukan</h1>
             </div>
         </div>
     )
